426cdca28daf857d999cc6b1a21a9c5fc9e96abb
Dalton Curtin <daltoncurtin@gmail.com>
2019-09-26 21:07:17 -0400

    Mostly working proof

--- log_computer_hours.ps1
+++ log_computer_hours.ps1
@@ -30,9 +30,9 @@ function getEventsForComputerLog
     if(Test-Connection $domainComputer -Quiet -Count 1)
     {
         [System.Collections.ArrayList] $LogonAndOff = @();
-        $startTime = (Get-Date).AddDays(-4);
-        $endTime = (Get-Date).AddDays(-1);
-        $LogonAndOffEvents = Get-WinEvent -ComputerName $domainComputer -FilterHashtable @{LogName='Security'; Id=@('4800','4801','4648'); StartTime=$startTime; EndTime=$endTime}
+        $startTime = (Get-Date).AddDays(-7);
+        $endTime = (Get-Date);
+        $LogonAndOffEvents = Get-WinEvent -ComputerName $domainComputer -FilterHashtable @{LogName='Security'; Id=@('4800','4801','4648','4647'); StartTime=$startTime; EndTime=$endTime}
         return $LogonAndOffEvents;
     }
     return $null;
@@ -47,7 +47,10 @@ function getADComputers
 
     [System.Collections.ArrayList] $domainComputers = @();
     Get-ADComputer -Properties @("Name","distinguishedName") -Filter *  | ForEach-Object -Process ({ if($_.distinguishedName -like "*Computer*$OULocaction*"){$null = $domainComputers.add($_.Name)}})
-    return $domainComputers;
+    $userIndex = $domainComputers.IndexOf('WHETSELL');
+    $user = $domainComputers[$userIndex];
+    return $domainComputers
+    #return @($user)
 }
 
 function generateUserRecords
@@ -76,13 +79,7 @@ function generateUserRecords
         #    $dateMapToRecords.add($currentDate, $_);
         #}
         #$rowTimeFormated.add($rowTimeFormated, $_);
-        if($dateMapToRecords.contains($rowTimeFormated))
-        {
-            $null = $dateMapToRecords[$rowTimeFormated].add($_);
-        }else
-        {
-            $null = $dateMapToRecords.add($rowTimeFormated, [System.Collections.ArrayList]@($_));
-        }
+        appendToListOfMap -map $dateMapToRecords -key $rowTimeFormated -value $_
         
     })
 
@@ -112,9 +109,61 @@ function getStartAndEndTime
         [System.Collections.ArrayList]$events,
         $date
     )
-    
-    [string]$userName = $null;
-    
+    $userNamesToEvents = [ordered]@{};
+    $userNamesToRecord = [ordered]@{};
+
+    $events | ForEach-Object -Process ({
+        $logEvent = $_;
+        $nameAndLog = getLoginLogOffEventFromUser -logEvent $logEvent -computerName $computerName
+        if($nameAndLog -ne $null)
+        {
+            appendToListOfMap -map $userNamesToEvents -key $nameAndLog.user -value $nameAndLog.event
+        }
+        
+    })
+
+    $userNamesToEvents.Keys | ForEach-Object -Process ({
+        $userName = $_;
+        $record = getStartAndEndTimeForAGivenUser -computer $computerName -userName $userName -events $userNamesToEvents[$userName] -date $date
+        if($record -ne $null)
+        {
+            appendToListOfMap -map $userNamesToRecord -key $userName -value $record
+        }
+    })
+    return $userNamesToRecord;
+
+}
+
+function getLoginLogOffEventFromUser
+{
+    param(
+        $logEvent,
+        [string]$computerName
+        )
+
+    if($logEvent.Id -eq 4648 -or $logEvent.Id -eq 4647 -or $logEvent.Id -eq 4801 -or $logEvent.Id -eq 4800)
+        {
+            $userName = IF ($logEvent.Id -eq 4648) {$logEvent.properties[5].Value} Else {$logEvent.properties[1].Value}
+
+            if($userName -ne "$computerName`$" -and -not ($userName -like "UMFD*") -and -not ($userName -like "DWM*"))
+            {
+                #Write-Host $userName $logEvent.Id
+                return @{'user'=$userName; 'event'=$logEvent};
+            }
+        }
+        return $null;
+}
+
+function getStartAndEndTimeForAGivenUser
+{
+    param
+    (
+        [string]$computer,
+        [string]$userName,
+        [System.Collections.ArrayList]$events,
+        $date
+    )
+     
     $eventCount = $events.count;
     $index = $eventCount - 1;
     #write-host $eventCount
@@ -125,11 +174,10 @@ function getStartAndEndTime
         #Write-Host "event id: $($events[$index].Id)"
         #Write-Host "Start date $index"
         $logEvent = $events[$index];
-        
+
         if($logEvent.Id -eq 4648 -or $logEvent.Id -eq 4801)
         {
-            $userName = IF ($logEvent.Id -eq 4648) {$logEvent.properties[5].Value} Else {$logEvent.properties[1].Value} 
-            $foundStartTime = IF ($userName -eq "$computerName`$" -or $userName -like "UMFD*" -or $userName -like "DWM*" ) {$null} Else {$logEvent.TimeCreated}
+            $foundStartTime = $logEvent.TimeCreated
         }
         $index--;
     }
@@ -141,7 +189,7 @@ function getStartAndEndTime
         #Write-Host "End date $index"
         $logEvent = $events[$index];
 
-        if($logEvent.Id -eq 4800)
+        if($logEvent.Id -eq 4800 -or $logEvent.Id -eq 4647)
         {
             $foundEndTime = $logEvent.TimeCreated;
 
@@ -159,20 +207,144 @@ function getStartAndEndTime
         return $null
     }
     write-host "startTime = $foundStartTime endtime = $foundEndTime"
-    return New-Object userRecord($userName, $foundStartTime, $foundEndTime, $date);    
+    $startTime = $foundStartTime.ToUniversalTime();
+    $endTime = $foundEndTime.ToUniversalTime();
+    return [UserRecord]::New($userName, $computer, $startTime, $endTime, $date);    
 }
 
-class userRecord
+function appendToListOfMap
+{
+    param($map,
+            $key,
+            $value
+        )
+
+            if($map.contains($key))
+            {
+                $null = $map[$key].add($value)
+            }else
+            {
+                $null = $map.add($key, [System.Collections.ArrayList] @($value));
+            }
+}
+
+function generateSFRecords
+{
+    param(
+        $records,
+        $json=$false
+    )
+
+    if($json -eq $true)
+    {
+        return generateSFRecordsAsJson -records $records
+    }else
+    {
+        return generateSFRecordsAsCSV -records $records
+    }
+}
+
+function generateSFRecordsAsJson
+{
+    param(
+        $records
+    )
+    $jsonGenerator = [SFRecordGenerator]::New($true);
+    return generateSFRecordWithGenerator -records $records -recordGenerator $jsonGenerator
+}
+
+function generateSFRecordsAsCSV
+{
+    param(
+        $records
+    )
+    $csvGenerator = [SFRecordGenerator]::New($false);
+    return generateSFRecordWithGenerator -records $records -recordGenerator $csvGenerator
+}
+
+function generateSFRecordWithGenerator
+{
+    param(
+        $records,
+        [SFRecordGenerator]$recordGenerator
+    )
+
+    $records.Keys | ForEach-Object -Process ({
+        $ComputerName = $_;
+        $datedRecords = $records[$ComputerName];
+
+        $datedRecords.Keys | ForEach-Object -Process ({
+            $date = $_;
+            $userRecords = $datedRecords[$date]
+
+            $userRecords.Keys | ForEach-Object -Process ({
+                $user = $_;
+                $userRecordFromDate = $($userRecords[$user]);
+
+                #[userRecord]$userRecordForCurrentDate = $($userRecord[0]);
+                $recordGenerator.GenerateRecord($userRecordFromDate);
+            })
+            
+        })
+    })
+    return $recordGenerator.getRecordsList();
+}
+
+class SFRecordGenerator
+{
+    $recordCount
+    $generateJson
+    [System.Collections.ArrayList]$generatedRecordList
+
+    SFRecordGenerator($generateJson)
+    {
+        $this.recordCount = 0;
+        $this.generatedRecordList = @();
+        $this.generateJson = $generateJson;
+    }
+
+    GenerateRecord([userRecord]$record)
+    {
+        if($this.generateJson)
+        {
+            $this.GenerateJson($record);
+        }else
+        {
+            $this.GenerateCSV($record);
+        }
+    }
+
+    GenerateJson([userRecord]$record)
+    {
+        $null = $this.generatedRecordList.Add($record.toJsonStructure($this.recordCount++));
+    }
+
+    GenerateCSV([userRecord]$record)
+    {
+        $null = $this.generatedRecordList.Add($record.toCSVStructure());
+    }
+
+    [System.Collections.ArrayList]getRecordsList()
+    {
+        return $this.generatedRecordList;
+    }
+}
+
+
+
+class UserRecord
 {
     [string]$userName
+    [string]$computer
     [System.DateTime]$startDate
     [System.DateTime]$endDate
     [string]$date
     [float]$hours
 
-    userRecord($userName, $startDate, $endDate, $date)
+    userRecord($userName, $computer, $startDate, $endDate, $date)
     {
         $this.userName = $userName;
+        $this.computer = $computer;
         $this.startDate = $startDate;
         $this.endDate = $endDate; 
         $this.date = $date;
@@ -185,11 +357,43 @@ class userRecord
         return '$this.userName $this.startDate $this.endDate $this.hours'
     }
 
+    [System.Collections.Hashtable]toJsonStructure([string] $refId)
+    {
+        $endDateForm = "$($this.endDate.Year)-$(([string]$this.endDate.Month).PadLeft(2,'0'))-$(([string]$this.endDate.Day).PadLeft(2,'0'))T$(([string]$this.endDate.Hour).PadLeft(2,'0')):$(([string]$this.endDate.Minute).PadLeft(2,'0')):$(([string]$this.endDate.Second).PadLeft(2,'0'))"
+        $startDateForm = "$($this.startDate.Year)-$(([string]$this.startDate.Month).PadLeft(2,'0'))-$(([string]$this.startDate.Day).PadLeft(2,'0'))T$(([string]$this.startDate.Hour).PadLeft(2,'0')):$(([string]$this.startDate.Minute).PadLeft(2,'0')):$(([string]$this.startDate.Second).PadLeft(2,'0'))"
+        $attributes = [ordered]@{type="computerLog__c"; referenceId="computerLog__cRef$refId"};
+        $recordDetails = [ordered]@{attributes=$attributes; Name="$($this.userName)-$($this.date)"; User_Name__c=$this.userName; Computer__c=$this.computer; Last_Logout__c=$endDateForm; First_Logon__c=$startDateForm; Hours__c=$this.hours}
+
+        return $recordDetails;
+    }
+
+    [string]toJson()
+    {
+        return $($this.toJsonStructure()|ConvertTo-Json);
+    }
+
+    [psobject]toCSVStructure()
+    {
+        $endDateForm = "$($this.endDate.Year)-$(([string]$this.endDate.Month).PadLeft(2,'0'))-$(([string]$this.endDate.Day).PadLeft(2,'0'))T$(([string]$this.endDate.Hour).PadLeft(2,'0')):$(([string]$this.endDate.Minute).PadLeft(2,'0')):$(([string]$this.endDate.Second).PadLeft(2,'0'))"
+        $startDateForm = "$($this.startDate.Year)-$(([string]$this.startDate.Month).PadLeft(2,'0'))-$(([string]$this.startDate.Day).PadLeft(2,'0'))T$(([string]$this.startDate.Hour).PadLeft(2,'0')):$(([string]$this.startDate.Minute).PadLeft(2,'0')):$(([string]$this.startDate.Second).PadLeft(2,'0'))"        
+        
+        [psobject]$record = New-Object psobject -Property $([ordered] @{Name="$($this.userName)-$($this.date)"; User_Name__c=$this.userName; Computer__c=$this.computer; Last_Logout__c=$endDateForm; First_Logon__c=$startDateForm; Hours__c=$this.hours})
+        return $record;
+    }
+
 }
 
-
 $adComputers = getADComputers -OULocaction "Fort Myers"
+#$processedMap = [ordered]@{WHETSELL=$([ordered]@{"2019-09-26"=$([ordered]@{bwhetsell=$([UserRecord]::new('testName','testComputer',$(Get-Date),$(Get-Date),'2019-09-26'))})})}
 $processedMap = processComputers -listOfComputers $adComputers
+$generatedSFRecords = $(generateSFRecords -records $processedMap -json $false) | ConvertTo-Csv -NoTypeInformation -OutVariable "ComputerLogs__.csv"
+
+
+
+#[IO.File]::WriteAllLines('ComputerLogs__c.json', $generatedSFRecords)
+#[IO.File]::WriteAllLines('ComputerLogs__c.csv', $generatedSFRecords)
+#sfdx force:data:bulk:upsert -s computerlog__c -f .\computerlogs__c.csv -i Name -u dcurtin@midlandira.com.dcurtin
+#sfdx force:data:tree:import -f .\ComputerLogs__c.json -u dcurtin@midlandira.com.dcurtin
 
 #$mapOfComputersToLog = generateMapOfComputersToLogEvents -domainComputers $adComputers
 
